78a52d57f77be8b270e1831bf7151cc5dcd896d4
Sergey <vinnronn@gmail.com>
2021-11-07 19:53:51 +0300

    Change collision's method

--- Source/PZ/WeaponC.cpp
+++ Source/PZ/WeaponC.cpp
@@ -86,7 +86,7 @@ void AWeapon::WeaponTrace()
 		false, 1.0f, 0, 0.5f);
 	
 	FHitResult OutHit;
-	GetWorld()->LineTraceSingleByChannel(OutHit, TraceStart, TraceEnd, ECollisionChannel::ECC_Visibility);
+	GetWorld()->LineTraceSingleByChannel(OutHit, TraceStart, TraceEnd, ECollisionChannel::ECC_WorldStatic);
 
 	if (OutHit.bBlockingHit)
 	{
