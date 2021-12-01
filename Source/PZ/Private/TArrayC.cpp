1d53a000d0d90fbdb3b10169df6620092a9c2e9e
Sergey Shuppo <vinnronn@gmail.com>
2021-12-01 09:26:47 +0300

    create TArray

---
+++ Source/PZ/Private/TArrayC.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TArrayC.h"
+
+// Sets default values
+ATArrayC::ATArrayC()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ATArrayC::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ATArrayC::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
